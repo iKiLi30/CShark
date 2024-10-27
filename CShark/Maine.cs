a9ac9cad1b27168d23c41f6fd78edfb7034b7183
iKiLi30 <jolahmilovski@gmail.com>
2024-10-27 16:41:53 -0400

    dell construct obj

--- CShark/Maine.cs
+++ CShark/Maine.cs
@@ -13,11 +13,9 @@ namespace CShark
             System.Console.WriteLine("Это фабрика");
             
             Logik Rlogik = new RoadLogik();
-            Transport truck = new Truck();
             Rlogik.Delivery();
 
             Logik Slogik = new SeaLogik();
-            Transport boath = new Boath();
             Slogik.Delivery();
             
         }
